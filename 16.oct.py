67437d4e2eff931cb88c0ab78ba704c4e2db176c
Mariss17 <marioberzins@gmail.com>
2023-10-16 10:12:58 +0300

    pārmaiņas

--- 16.oct.py
+++ 16.oct.py
@@ -1,7 +1,16 @@
 #git tutoriel
-#izdrukā pāra skaitļus no 130-150
-
+#1uzd. izdrukā pāra skaitļus no 130-150
+"""
 for skaitlis in range(130,151):
     if skaitlis %2==0:
         print(skaitlis)
+"""
+#2uzd izdrukā skaitļus no 15-89 kas dalās at 5, 3 un 2
 
+for skaitlis in range(15,90):
+    if skaitlis %2==0:
+        print(skaitlis)
+    elif skaitlis %3==0:
+        print(skaitlis)
+    elif skaitlis %5==0:
+        print(skaitlis)
